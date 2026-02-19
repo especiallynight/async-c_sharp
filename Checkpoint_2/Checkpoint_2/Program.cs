d5b5047d0b8323ade39c72b336436bb9c05aba4a
alina <degtyareva_18@mail.ru>
2026-02-19 17:08:05 +0300

    first commit

---
+++ Checkpoint_2/Checkpoint_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using (var pipeServer = new NamedPipeServerStream("TestPipe", PipeDirection.In))
+        {
+            Console.WriteLine("Ожидание подключения клиента...");
+            await pipeServer.WaitForConnectionAsync();
+            Console.WriteLine("Клиент подключен.");
+            using (var reader = new BinaryReader(pipeServer, Encoding.UTF8, leaveOpen: true))
+            {
+                while (true)
+                {
+                    try
+                    {
+                        string message = reader.ReadString();
+                        Console.WriteLine($"Получено сообщение: {message}");
+                        
+                        long fileLength = reader.ReadInt64();
+                        string fileName = reader.ReadString();
+                        using (var fileStream = File.Create(fileName))
+                        {
+                            byte[] buffer = new byte[81920];
+                            long totalRead = 0;
+
+                            while (totalRead < fileLength)
+                            {
+                                int toRead = (int)Math.Min(buffer.Length, fileLength - totalRead);
+                                int bytesRead = pipeServer.Read(buffer, 0, toRead);
+                                fileStream.Write(buffer, 0, bytesRead);
+                                totalRead += bytesRead;
+                            }
+                            
+                        }
+                        Console.WriteLine("Содержимое файла: " + File.ReadAllText(fileName, Encoding.UTF8));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Ошибка: {ex.Message}");
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ Checkpoint_2_2/Checkpoint_2_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+
+    static async Task Main(string[] args)
+    {
+        using (var pipeClient = new NamedPipeClientStream(".", "TestPipe", PipeDirection.Out))
+        {
+            Console.WriteLine("Подключение к серверу...");
+            await pipeClient.ConnectAsync();
+            Console.WriteLine("Подключено к серверу.");
+            using (var writer = new BinaryWriter(pipeClient))
+            {
+
+                while (true)
+                {
+                    Console.Write("Введите сообщение: ");
+                    string message = Console.ReadLine();
+                    writer.Write(message);
+                    if (message.Equals("Выйти"))
+                    {
+                        break;
+                    }
+
+
+                    Console.Write("Введите путь файла: ");
+                    string filepath = Console.ReadLine();
+                    if (!File.Exists(filepath))
+                    {
+                        Console.WriteLine("Файл не найден.");
+                        continue;
+                    }
+
+                    FileInfo fileInfo = new FileInfo(filepath);
+                    writer.Write(fileInfo.Length);
+                    writer.Write(fileInfo.Name);
+                    using (var filestream = File.OpenRead(filepath))
+                    {
+                        filestream.CopyTo(pipeClient);
+                    }
+                }
+            }
+        }
+    }
+}
